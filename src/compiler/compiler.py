2a4eb79f46c4eb569892ecddd45f65d61ee28f54
Jacob Börstell <jacob.borstell@outlook.com>
2026-04-26 19:07:49 +0200

    minor refactorings

--- src/compiler/compiler.py
+++ src/compiler/compiler.py
@@ -2,7 +2,7 @@ from lexer import Lexer
 from parser import parse
 
 def test():
-    with open("examples/test.jcsr", "r", encoding="utf-8") as file:
+    with open("examples/test.jacscript", "r", encoding="utf-8") as file:
         data = file.read()
 
     tokens = Lexer.tokenize(data)
