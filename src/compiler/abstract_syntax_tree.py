de2b8c18c3c755761b50ce4bd79241510592cec1
Jacob Börstell <jacob.borstell@outlook.com>
2026-04-26 11:43:50 +0200

    Some refactoring

--- src/compiler/abstract_syntax_tree.py
+++ src/compiler/abstract_syntax_tree.py
@@ -1,66 +1,16 @@
 from typing import Any, Optional
-from abc import ABC, abstractmethod
 
-class AbstractTreeNode(ABC):
-    @abstractmethod
-    def __init__(self, data: Any, parent: AbstractTreeNode | None) -> None:
-        raise NotImplementedError
+class AbstractTreeNode:
+    def __init__(self, parent: AbstractTreeNode | None) -> None:
+        self.parent = parent
 
-    @abstractmethod
-    def get_child(self, key) -> AbstractTreeNode | None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_children(self) -> list[AbstractTreeNode] | dict[Any, AbstractTreeNode]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def create_child(self, child_class: type[AbstractTreeNode], child_data: Any,
-                     key: Optional[Any] = None) -> AbstractTreeNode:
-        raise NotImplementedError
-
-
-
-class ListLikeTreeNode(AbstractTreeNode):
-    def __init__(self, data, parent: AbstractTreeNode | None):
-        self.data = data
-        self.parent: AbstractTreeNode | None = parent
-        self.children: list[AbstractTreeNode] = []
-
-    def get_child(self, key) -> AbstractTreeNode | None:
-        return self.children[key]
-
-    def get_children(self) -> list[AbstractTreeNode]:
-        return self.children
-
-    def create_child(self, child_class: type[AbstractTreeNode], child_data: Any,
-                     key: Optional[Any] = None) -> AbstractTreeNode:
-        child = child_class(child_data, parent=self)
-        self.children.append(child)
+    def create_child(self, child_class: type[AbstractTreeNode], *child_data: Any) -> AbstractTreeNode:
+        child = child_class(*child_data, parent=self)
         return child
 
 
 
-class DictLikeTreeNode(AbstractTreeNode):
-    def __init__(self, data, parent: AbstractTreeNode | None):
-        self.data = data
-        self.parent: AbstractTreeNode | None = parent
-        self.children: dict[Any, AbstractTreeNode] = {}
-
-    def get_child(self, key) -> AbstractTreeNode | None:
-        return self.children[key]
-
-    def get_children(self) -> dict[Any, AbstractTreeNode]:
-        return self.children
-
-    def create_child(self, child_class: type[AbstractTreeNode], child_data: Any,
-                     key: Optional[Any] = None) -> AbstractTreeNode:
-        child = child_class(child_data, parent=self)
-        self.children[key] = child
-        return child
-
-
-class AbstractSyntaxTree(ListLikeTreeNode):
+class AbstractSyntaxTree(AbstractTreeNode):
     def __init__(self):
         super().__init__([], None)
 
